5c0e11d3f3753cecfa3cbcc4c8cc98a9988944b6
OmarElEssiely <dev.omar@outlook.sa>
2017-06-02 14:56:04 +0200

    Presentation Updated

--- Material Tracking/Presentation/Properties/AssemblyInfo.cs
+++ Material Tracking/Presentation/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f3a3d1da-7234-4ebf-9697-b9529099f68d")]
+[assembly: Guid("edf5c950-0ef1-4554-982a-5593ad5f074a")]
 
 // Version information for an assembly consists of the following four values:
 //
